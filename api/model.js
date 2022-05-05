14b54f01b345e5f1c9031e49cf33e6e7be4a2095
Ogriceboy <haoanhtran7@gmail.com>
2022-05-05 13:59:11 -0700

    added basic parsing functionality

--- api/model.js
+++ api/model.js
@@ -8,6 +8,52 @@ const userFormData = new Schema({
     },
 
     Question2: {
+        type: Number
+    },
+
+    Question3: {
+        type: Number
+    },
+
+    Question4: {
+        type: String
+    },
+
+    Question5: {
+        type: String
+    },
+
+    Question6: {
+        type: String
+    },
+
+    Question7: {
+        type: String
+    },
+
+    Question8: {
+        type: String
+    },
+
+    Question9: {
+        type: String
+    },
+
+    Question10: {
+        type: Number
+    },
+
+    Question11: {
+        type: Number
+    },
+
+    Question12: {
+        type: Number
+    },
+
+    Question13: {
         type: String
     }
 })
+
+module.exports = userFormData; 
--- api/server.js
+++ api/server.js
@@ -12,6 +12,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/form-data", {
     .then( () => console.log("Connected to DB"))
     .catch(console.error)
 
+const userFormData = require(".model")
+
 app.use(express.json())
 app.use(cors())
 
@@ -19,7 +21,34 @@ app.get("/", (req, res) => {
     res.json("Hello world")
 })
 
-app.post("/hook", (req, res) => {
+app.post("/hook", async (req, res) => {
+
+    //parse answers from the response
+    answers = req.body.form_response.answers
+    n = Object.keys(answers).length
+    result = []
+
+    for(var i = 0; i < n; i++){
+        if(answers[i].type == "choice"){
+            result.push(answers[i].choice.label)
+        }
+        if(answers[i].type == "number"){
+            result.push(answers[i].number)
+        }
+    }
+
+    console.log(results)
+
+    //initialize user form data schema 
+
+    //add schema to database 
+
+
+
+
+
+
+
     res.json("Payload processed")
 })
 
